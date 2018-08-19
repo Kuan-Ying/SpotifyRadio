67e17c985d286a1c5c00eea199579462071e79ef
Kuan Ying Chen <kuanying.chen@mvpfastlane.com>
2018-08-18 19:53:48 -0700

    remove font weight

--- src/components/SpotifyPlayer/TrackList/Text.js
+++ src/components/SpotifyPlayer/TrackList/Text.js
@@ -4,7 +4,6 @@ import { List } from 'semantic-ui-react';
 
 const Text = styled(({ isPlaying, ...props }) => <List.Content {...props} />)`
   color: ${({ isPlaying, activecolor, inactivecolor }) => (isPlaying ? activecolor : inactivecolor)};
-  font-weight: ${({ isPlaying }) => (isPlaying ? 'bold' : 'normal')};
 `;
 
 export default Text;
